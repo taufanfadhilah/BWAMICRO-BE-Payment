e55bf50eb9b9cb6c38d1f1c17c66a576d6056436
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-11-24 08:14:17 +0700

    webhook for midtrans

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -36,7 +36,7 @@ class OrderController extends Controller
         ]);
 
         $transactionDetail = [
-            'order_id' => $order->id.Str::random(5),
+            'order_id' => $order->id.'-'.Str::random(5),
             'gross_amount' => $course['price']
         ];
 
--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PaymentLog;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class WebhookController extends Controller
@@ -28,6 +30,69 @@ class WebhookController extends Controller
                 'data' => null
             ], 400);
         }
-        return true;
+
+        $realOrderId = explode('-', $orderId);
+        $order = Order::find($realOrderId);
+
+        if (!$order) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'order not found',
+                'data' => null
+            ], 404);
+        }
+
+        if ($order->status === 'success') {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'operation not permitted',
+                'data' => null
+            ], 405);
+        }
+
+        if ($transactionStatus == 'capture'){
+            if ($fraudStatus == 'challenge'){
+                // TODO set transaction status on your database to 'challenge'
+                // and response with 200 OK
+                $order->status = 'challenge';
+            } else if ($fraudStatus == 'accept'){
+                // TODO set transaction status on your database to 'success'
+                // and response with 200 OK
+                $order->status = 'success';
+            }
+        } else if ($transactionStatus == 'settlement'){
+            // TODO set transaction status on your database to 'success'
+            // and response with 200 OK
+            $order->status = 'success';
+        } else if ($transactionStatus == 'cancel' ||
+          $transactionStatus == 'deny' ||
+          $transactionStatus == 'expire'){
+          // TODO set transaction status on your database to 'failure'
+          // and response with 200 OK
+          $order->status = 'failure';
+        } else if ($transactionStatus == 'pending'){
+          // TODO set transaction status on your database to 'pending' / waiting payment
+          // and response with 200 OK
+          $order->status = 'pending';
+        }
+
+        $logsData = [
+            'status' => $transactionStatus,
+            'raw_response' => json_encode($data),
+            'order_id' => $realOrderId,
+            'payment_type' => $type
+        ];
+
+        PaymentLog::create($logsData);
+        $order->save();
+
+        if ($order->status === 'success') {
+            createPremiumAccess([
+                'user_id' => $order->user_id,
+                'course_id' => $order->course_id
+            ]);
+        }
+
+        return response()->json('Ok');
     }
 }
